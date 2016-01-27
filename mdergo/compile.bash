86e1b2850b6d4fc93d62056843b72be4726a49b7
adamsaj <adamsaj@me.com>
2016-01-26 20:08:49 -0600

    updates compile.bash to abstract deal with controller firmware

--- mdergo/compile.bash
+++ mdergo/compile.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-cp ./mdergo* ../../controller/kll/layouts
-cp ./ergodox.bash ../../controller/Keyboards
+cd ../../controller
+git pull
+cd ..
+cp keyboard_layouts/mdergo/mdergo* controller/kll/layouts
+cp keyboard_layouts/mdergo/ergodox.bash controller/Keyboards
 
-cd ../../controller/Keyboards
+cd controller/Keyboards
 ./ergodox.bash
+cd ../..
+mv controller/Keyboards/ICED-* keyboard_layouts/mdergo
